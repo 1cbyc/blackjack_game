fa49a320991ed705c28c370e8b4f155c3e583c47
Isaac Emmanuel <isaacnsisong@gmail.com>
2024-12-30 00:21:09 +0100

    Changes made on 2024-12-30 00:21:09

--- src/utils.py
+++ src/utils.py
@@ -24,3 +24,26 @@
 #     else:
 #         print(*[f"{card['value']} of {card['suit']}" for card in hand])
 # """)
+def calculate_score(hand):
+    score = 0
+    aces = 0
+    for card in hand:
+        if card['value'] in ['Jack', 'Queen', 'King']:
+            score += 10
+        elif card['value'] == 'Ace':
+            aces += 1
+            score += 11
+        else:
+            score += int(card['value'])
+
+    while score > 21 and aces:
+        score -= 10
+        aces -= 1
+
+    return score
+
+def display_hand(hand, hide_first_card=False):
+    if hide_first_card:
+        print("[Hidden Card],", *[f"{card['value']} of {card['suit']}" for card in hand[1:]])
+    else:
+        print(*[f"{card['value']} of {card['suit']}" for card in hand])
